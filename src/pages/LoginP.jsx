f50c73e75e71847561d89ad03dd7c336485e580c
prayag <prayag.bhosale@smartdings.com>
2025-01-10 22:40:01 +0530

    fix: check need of navigating to /home before rendering login pahe

--- src/pages/LoginP.jsx
+++ src/pages/LoginP.jsx
@@ -111,164 +111,172 @@ function Login() {
   };
 
   return (
-    <div className="flex flex-col-reverse lg:flex-row h-full md:px-1 lg:px-16 lg:h-screen bg-gray-50/10">
-      <div className="hidden md:flex flex-col gap-2 w-1/2 bg-cover bg-center items-center justify-center">
-        <img
-          src={LoginImg}
-          alt="Login"
-          className="animate-float-slow origin-top object-cover h-[500px] w-[500px]"
-        />
-        <div className="">
-          <Quotes />
-        </div>
-      </div>
-      <div className="flex items-center justify-center w-full md:w-1/2">
-        <div className="max-w-xl w-full">
-          <div className="bg-white shadow-none lg:shadow-lg rounded-2xl p-8">
-            <HeaderTitle />
-            <div className="space-y-3">
-              <button className="custom-button">
-                <img src={GoogleImg} alt="Google" className="w-6 h-6" />
-                <span>Login with Google</span>
-              </button>
-              <button className="custom-button">
-                <img src={FaceBookImg} alt="FaceBook" />
-                <span>Login with Facebook</span>
-              </button>
+    <>
+      {!localStorage.getItem("authToken") && (
+        <div className="flex flex-col-reverse lg:flex-row h-full my-10 md:px-1 lg:px-16 bg-gray-50/10">
+          <div className="hidden md:flex flex-col gap-2 w-1/2 bg-cover bg-center items-center justify-center">
+            <img
+              src={LoginImg}
+              alt="Login"
+              className="animate-float-slow origin-top object-cover h-[500px] w-[500px]"
+            />
+            <div className="">
+              <Quotes />
             </div>
-            <div className="flex items-center my-6">
-              <div className="border-t flex-grow border-gray-200"></div>
-              <span className="px-4 text-gray-500 text-sm">OR</span>
-              <div className="border-t flex-grow border-gray-200"></div>
-            </div>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
-                <div className="p-2 pr-1">
-                  <img
-                    src={AccountIcon}
-                    alt="Profile"
-                    className="w-6 h-6 rounded-full"
-                  />
+          </div>
+          <div className="flex items-center justify-center w-full md:w-1/2">
+            <div className="max-w-xl w-full">
+              <div className="bg-white shadow-none lg:shadow-lg rounded-2xl p-8">
+                <HeaderTitle />
+                <div className="space-y-3">
+                  <button className="custom-button">
+                    <img src={GoogleImg} alt="Google" className="w-6 h-6" />
+                    <span>Login with Google</span>
+                  </button>
+                  <button className="custom-button">
+                    <img src={FaceBookImg} alt="FaceBook" />
+                    <span>Login with Facebook</span>
+                  </button>
                 </div>
-                <div className="flex flex-col px-4 py-2 w-full">
-                  <label className="text-sm font-semibold text-gray-600">
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    name="username"
-                    value={userLogin.username}
-                    onChange={handleChange}
-                    placeholder="Enter Username"
-                    className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
-                  />
-                  {errors.username && (
-                    <p className="text-red-500 text-sm">{errors.username}</p>
-                  )}
+                <div className="flex items-center my-6">
+                  <div className="border-t flex-grow border-gray-200"></div>
+                  <span className="px-4 text-gray-500 text-sm">OR</span>
+                  <div className="border-t flex-grow border-gray-200"></div>
                 </div>
-              </div>
-              <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
-                <div className="p-2 pr-1">
-                  <img
-                    src={MailIcon}
-                    alt="Profile"
-                    className="w-6 h-6 rounded-full"
-                  />
-                </div>
-                <div className="flex flex-col px-4 py-2 w-full">
-                  <label className="text-sm font-semibold text-gray-600">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={userLogin.email}
-                    onChange={handleChange}
-                    placeholder="Enter Email"
-                    className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
-                  />
-                  {errors.email && (
-                    <p className="text-red-500 text-sm">{errors.email}</p>
-                  )}
-                </div>
-              </div>
-              <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
-                <div className="p-2 pr-1">
-                  <img
-                    src={KeyIcon}
-                    alt="Profile"
-                    className="w-6 h-6 rounded-full"
-                  />
-                </div>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
+                    <div className="p-2 pr-1">
+                      <img
+                        src={AccountIcon}
+                        alt="Profile"
+                        className="w-6 h-6 rounded-full"
+                      />
+                    </div>
+                    <div className="flex flex-col px-4 py-2 w-full">
+                      <label className="text-sm font-semibold text-gray-600">
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        name="username"
+                        value={userLogin.username}
+                        onChange={handleChange}
+                        placeholder="Enter Username"
+                        className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
+                      />
+                      {errors.username && (
+                        <p className="text-red-500 text-sm">
+                          {errors.username}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
+                    <div className="p-2 pr-1">
+                      <img
+                        src={MailIcon}
+                        alt="Profile"
+                        className="w-6 h-6 rounded-full"
+                      />
+                    </div>
+                    <div className="flex flex-col px-4 py-2 w-full">
+                      <label className="text-sm font-semibold text-gray-600">
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={userLogin.email}
+                        onChange={handleChange}
+                        placeholder="Enter Email"
+                        className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
+                      />
+                      {errors.email && (
+                        <p className="text-red-500 text-sm">{errors.email}</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex items-center bg-[#F4F4F4] border-gray-200 rounded-xl">
+                    <div className="p-2 pr-1">
+                      <img
+                        src={KeyIcon}
+                        alt="Profile"
+                        className="w-6 h-6 rounded-full"
+                      />
+                    </div>
 
-                <div className="flex flex-col px-4 py-2 w-full">
-                  <label className="text-sm font-semibold text-gray-600">
-                    Password
-                  </label>
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    value={userLogin.password}
-                    onChange={handleChange}
-                    placeholder="Enter Password"
-                    className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
-                  />
-                  {errors.password && (
-                    <p className="text-red-500 text-sm">{errors.password}</p>
+                    <div className="flex flex-col px-4 py-2 w-full">
+                      <label className="text-sm font-semibold text-gray-600">
+                        Password
+                      </label>
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        value={userLogin.password}
+                        onChange={handleChange}
+                        placeholder="Enter Password"
+                        className="w-full bg-transparent font-semibold border-0 outline-none text-gray-800"
+                      />
+                      {errors.password && (
+                        <p className="text-red-500 text-sm">
+                          {errors.password}
+                        </p>
+                      )}
+                    </div>
+                    <div className="p-2 mr-1">
+                      <img
+                        src={showPassword ? VisibilityIcon : VisibilityOffIcon}
+                        alt="Profile"
+                        className="w-6 h-6 rounded-full"
+                        onClick={togglePasswordVisibility}
+                      />
+                    </div>
+                  </div>
+                  {errors.apiError && (
+                    <p className="text-red-500 text-sm">{errors.apiError}</p>
                   )}
-                </div>
-                <div className="p-2 mr-1">
-                  <img
-                    src={showPassword ? VisibilityIcon : VisibilityOffIcon}
-                    alt="Profile"
-                    className="w-6 h-6 rounded-full"
-                    onClick={togglePasswordVisibility}
-                  />
-                </div>
-              </div>
-              {errors.apiError && (
-                <p className="text-red-500 text-sm">{errors.apiError}</p>
-              )}
-              <div className="flex justify-between items-center my-4">
-                <label className="flex items-center">
-                  <input type="checkbox" className="mr-2" />
-                  <span>Remember Me</span>
-                </label>
-                <a
-                  href="#"
-                  className="text-indigo-600 hover:underline"
-                  onClick={() =>
-                    alert(
-                      "Oops! Looks like your password took a vacation! 🌴\nBut don’t worry, I’ll be joining UNSTOP soon to help you reset it. 😎"
-                    )
-                  }
-                >
-                  Forgot Password?
-                </a>
-              </div>
-              <button
-                type="submit"
-                className={`w-full py-2.5  hover:bg-[#5347cb] text-white rounded-lg transition-colors
+                  <div className="flex justify-between items-center my-4">
+                    <label className="flex items-center">
+                      <input type="checkbox" className="mr-2" />
+                      <span>Remember Me</span>
+                    </label>
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:underline"
+                      onClick={() =>
+                        alert(
+                          "Oops! Looks like your password took a vacation! 🌴\nBut don’t worry, I’ll be joining UNSTOP soon to help you reset it. 😎"
+                        )
+                      }
+                    >
+                      Forgot Password?
+                    </a>
+                  </div>
+                  <button
+                    type="submit"
+                    className={`w-full py-2.5  hover:bg-[#5347cb] text-white rounded-lg transition-colors
                                     ${
                                       loading ? "bg-[#786df6]" : "bg-[#6255f7]"
                                     }`}
-              >
-                {loading ? "Logging in...." : "Login"}
-              </button>
-            </form>
-            <p className="text-center mt-6 text-sm text-gray-600">
-              Don't have an account?{" "}
-              <a
-                href="#"
-                className="text-indigo-600 hover:text-indigo-700 font-medium"
-              >
-                Register
-              </a>
-            </p>
+                  >
+                    {loading ? "Logging in...." : "Login"}
+                  </button>
+                </form>
+                <p className="text-center mt-6 text-sm text-gray-600">
+                  Don't have an account?{" "}
+                  <a
+                    href="#"
+                    className="text-indigo-600 hover:text-indigo-700 font-medium"
+                  >
+                    Register
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
