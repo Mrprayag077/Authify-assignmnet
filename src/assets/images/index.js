3a1c36bf859ee5d581f89a7b7ec879163c542acc
prayag <prayag.bhosale@smartdings.com>
2025-01-10 22:25:38 +0530

    create: assets structure

---
+++ src/assets/images/index.js
@@ -0,0 +1,14 @@
+export { default as GoogleImg } from "./google.png";
+export { default as FaceBookImg } from "./facebook.png";
+export { default as ProfileImg } from "./profile.png";
+export { default as KeyIcon } from "./key.svg";
+export{default as MailIcon} from './mail.svg'
+export { default as VisibilityIcon } from "./visibility.svg";
+export { default as VisibilityOffIcon } from "./visibility_off.svg";
+export { default as LoginImg } from "./login.png";
+export { default as AccountIcon } from "./account.svg";
+
+export { default as LinkedInIcon } from "./linkedin.svg";
+export { default as GithubIcon } from "./github.svg";
+export { default as PortfolioIcon } from "./portfolio.svg";
+export { default as CoderIcon } from "./coder.svg";
