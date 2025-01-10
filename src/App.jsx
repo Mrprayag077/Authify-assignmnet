823ba2daa4862893df82c8b21df585b7ac7736e8
prayag <prayag.bhosale@smartdings.com>
2025-01-10 22:25:59 +0530

    create: routing for application

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { ProtectedRoute } from "./component";
+import { Login, Home, NotFound } from "./pages";
+import useCheckProductionMode from "./hooks/useCheckProductionMode";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/auth/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
